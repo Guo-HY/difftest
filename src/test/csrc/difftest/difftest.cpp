dfa9ef4c212d0ee4d74134ad1f14ac123b2c50f5
ghy <2247883756@qq.com>
2023-07-26 21:19:34 +0800

    update ibar inst hint

--- src/test/csrc/difftest/difftest.cpp
+++ src/test/csrc/difftest/difftest.cpp
@@ -643,7 +643,7 @@ int Difftest::check_timeout() {
   // check whether there're any commits in the last 5000 cycles
   if (has_commit && ticks > last_commit + stuck_limit) {
     eprintf("No instruction of core %d commits for %lu cycles, maybe get stuck\n"
-        "(please also check whether a fence.i instruction requires more than %lu cycles to flush the icache)\n",
+        "(please also check whether a ibar instruction requires more than %lu cycles to flush the icache and dcache)\n",
         id, stuck_limit, stuck_limit);
     eprintf("Let REF run one more instruction.\n");
     proxy->exec(1);
